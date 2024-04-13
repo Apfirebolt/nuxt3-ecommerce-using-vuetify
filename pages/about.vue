387d4e80166dbecfaa91dd78403e11b983e0bd6d
Apfirebolt <aspper20@gmail.com>
2024-04-13 19:49:56 +0530

    added interceptor utils

--- pages/about.vue
+++ pages/about.vue
@@ -1,16 +1,52 @@
 <template>
   <div>
     <p>About Page</p>
+    <p v-if="isLoading">
+      Loading ...
+    </p>
+    <v-container v-else>
+      <v-row>
+        <v-col>
+          <v-card v-for="item in data" :key="item.id">
+            <v-card-title>
+              {{ item.title }}
+            </v-card-title>
+            <v-card-text>
+              {{ item.description }}
+            </v-card-text>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
   </div>
 </template>
 <script setup>
-const isOpen = ref(false);
 import { useCounterStore } from "~/store/counter"; // Assuming auto-imports
+import { ref, onMounted } from "vue";
+import httpClient from "~/utils/interceptor";
 
+const isOpen = ref(false);
+const data = ref(null);
+const isLoading = ref(false);
 const counterStore = useCounterStore();
 
-onMounted(() => {
+const getApiData = async () => {
+  try {
+    isLoading.value = true;
+    const response = await httpClient.get(`anime`);
+    if (response.status === 200) {
+      data.value = response.data.data;
+    }
+  } catch (error) {
+    console.error(error);
+  } finally {
+    isLoading.value = false;
+  }
+};
+
+onMounted(async () => {
   console.log("Mounted ..");
+  await getApiData();
   console.log("Counter Value:", counterStore.count);
 });
 </script>
---
+++ utils/interceptor.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+let baseURL = 'https://api.jikan.moe/v4/';
+
+const httpClient = axios.create({ baseURL });
+
+// Create a request interceptor
+
+const requestInterceptor = httpClient.interceptors.request.use(
+    config => {
+        // Do something before request is sent
+        return config;
+    },
+    error => {
+        // Do something with request error
+        console.log('Error here')
+        return Promise.reject(error);
+    }
+);
+
+// Create a response interceptor
+const responseInterceptor = httpClient.interceptors.response.use(
+    response => {
+        return response;
+    },
+    error => {
+        // Do something with response error
+        return Promise.reject(error);
+    }
+);
+
+export default httpClient;
