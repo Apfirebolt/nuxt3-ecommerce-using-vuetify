87d39fa2d3b499cfffebb91b47a17ba79629fa6a
Apfirebolt <aspper20@gmail.com>
2024-04-13 22:44:25 +0530

    custom css for navbar

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,26 +1,11 @@
 <template>
-  <v-footer color="primary" app>
+  <v-footer color="success" app>
     <v-container>
       <v-row>
         <v-col cols="12" sm="6">
           <v-card-title class="white--text"> Footer Content </v-card-title>
           <v-card-text class="white--text">
             This is a Vuetify footer example.
-          </v-card-text>
-        </v-col>
-        <v-col cols="12" sm="6">
-          <v-card-title class="white--text"> Links </v-card-title>
-          <v-card-text class="white--text">
-            <v-list>
-              <v-list-item
-                v-for="(link, index) in links"
-                :key="index"
-                link
-                class="white--text"
-              >
-                {{ link }}
-              </v-list-item>
-            </v-list>
           </v-card-text>
         </v-col>
       </v-row>
@@ -30,6 +15,4 @@
 
 <script setup>
 import { ref } from "vue";
-
-const links = ref(["Home", "About", "Contact"]);
 </script>
--- components/Header.vue
+++ components/Header.vue
@@ -1,14 +1,62 @@
 <template>
-    <header>
+  <header>
+    <nav>
       <div>
-        <!-- previously set width 1600 height 1200 -->
-        <p>
-            This is a footer
-        </p>
+        <h3>
+          NUXT ECommerce 
+        </h3>
       </div>
-    </header>
-  </template>
-  <script setup>
-  
-  </script>
-  
+      <div>
+        <NuxtLink to="/items" class="link">Item</NuxtLink>
+        <NuxtLink to="/" class="link">Home</NuxtLink>
+        <NuxtLink to="/about" class="link">About</NuxtLink>
+      </div>
+    </nav>
+  </header>
+</template>
+<script setup>
+
+import { ref } from 'vue'
+const drawer = ref(false)
+const menu = ref([
+  { title: 'Home', icon: 'mdi-home' },
+  { title: 'About', icon: 'mdi-information' },
+  { title: 'Contact', icon: 'mdi-email' },
+])
+
+const toggleMenu = () => {
+  console.log('Toggle')
+  drawer.value = !drawer.value
+}
+
+</script>
+
+<style>
+nav {
+  background-color: aquamarine;
+  padding: 1rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.link {
+  padding: 0.50rem 0.75rem;
+  border-radius: 2%;
+  box-shadow: 0 0 5px white;
+  margin: auto 0.5rem;
+  background-color: blue;
+  color: white;
+  text-decoration: none;
+  min-width: 100px;
+  text-align: center;
+  font-size: 1rem;
+  transition: all 0.3s ease-in-out;
+}
+
+.link:hover {
+  color: black;
+  background-color: white;
+  transition: all 0.3s ease-in-out;
+}
+</style>
